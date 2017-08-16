5b8411cb4ca3c15ab6928b5953989926a67dc8c9
bislewl <bislewl@gmail.com>
2017-08-16 14:39:16 -0500

    added a bunch of functions and got it actually working

--- includes/classes/clearcheckbook_api.php
+++ includes/classes/clearcheckbook_api.php
@@ -14,7 +14,22 @@ class clearcheckbook_api
     public function apiRequest($function, $params = array())
     {
         $function = preg_split('/(?=[A-Z])/', $function);
-        $method = strtoupper($function[0]);
+        switch ($function[0]) {
+            case 'insert':
+                $method = "POST";
+                break;
+            case 'edit':
+                $method = "PUT";
+                break;
+            case 'delete':
+                $method = "DELETE";
+                break;
+            case 'get':
+            default:
+                $method = "GET";
+                break;
+        }
+
         $call = strtolower($function[1]);
         $url = 'https://' . $this->username . ':' . $this->password . '@www.clearcheckbook.com/api/2.5/' . $call . '/?app_reference=' . $this->api_ref;
         $curl = curl_init($url);
@@ -27,10 +42,13 @@ class clearcheckbook_api
         }
 
         $response = curl_exec($curl);
-        if (!$response) {
-            die("Connection Failure");
-        }
+        curl_close($curl);
         $return = json_decode($response, true);
+        if (!$response) {
+            $return['error_msg'] = "Connection Failure";
+            $return['status'] = false;
+        }
+
         return $return;
     }
 
@@ -52,21 +70,30 @@ class clearcheckbook_api
     // Accounts
 
     // getAccounts - Gets all of the current user's accounts
-    public function getAccounts()
+    public function getAccounts($is_overview = false)
     {
-
+        $request['is_overview'] = $is_overview;
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // getAccount - Gets a single account
-    public function getAccount()
+    public function getAccount($id)
     {
-
+        $request['id'] = $id;
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // insertAccount - Adds an account for the current user
-    public function insertAccount()
+    public function insertAccount($name, $type_id, $balance = 0.00, $currency = 'USD')
     {
-
+        $request['name'] = $name;
+        $request['type_id'] = $type_id;
+        if (isset($balance) && $balance != '') $request['initial_balance'] = $balance;
+        if (isset($currency) && $currency != '') $request['currency_id'] = $balance;
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // editAccount - Edits a current user's account
@@ -112,6 +139,7 @@ class clearcheckbook_api
     // getTransactions - Gets all of the current user's transactions
     public function getTransactions($params = array())
     {
+        $request = array();
         if (isset($params['account_id']) && $params['account_id'] != '') $request['account_id'] = $params['account_id'];
         if (isset($params['created_at']) && $params['created_at'] != '') $request['created_at'] = $params['created_at'];
         if (isset($params['from_id']) && $params['from_id'] != '') $request['from_id'] = $params['from_id'];
@@ -119,18 +147,18 @@ class clearcheckbook_api
         if (isset($params['created_at_timezone']) && $params['created_at_timezone'] != '') $request['created_at_timezone'] = $params['created_at_timezone'];
         if (isset($params['page']) && $params['page'] != '') $request['page'] = $params['page'];
         if (isset($params['limit']) && $params['limit'] != '') $request['limit'] = $params['limit'];
-        if (isset($params['order']) && $params['order'] != ''){
-            $order_vars = array('date','created_at','amount','account','category','description','memo','payee','check_num');
+        if (isset($params['order']) && $params['order'] != '') {
+            $order_vars = array('date', 'created_at', 'amount', 'account', 'category', 'description', 'memo', 'payee', 'check_num');
             $order = strtolower($params['order']);
-            if(in_array($order,$order_vars)){
+            if (in_array($order, $order_vars)) {
                 $request['order'] = $params['order'];
             }
         }
 
-        if (isset($params['order_direction']) && $params['order_direction'] != ''){
-            $order_dir_vars = array('ASC','DESC');
+        if (isset($params['order_direction']) && $params['order_direction'] != '') {
+            $order_dir_vars = array('ASC', 'DESC');
             $order_dir = strtoupper($params['order_direction']);
-            if(in_array($order_dir,$order_dir_vars)){
+            if (in_array($order_dir, $order_dir_vars)) {
                 $request['order_direction'] = $params['order_direction'];
             }
         }
@@ -142,27 +170,66 @@ class clearcheckbook_api
     }
 
     // getTransaction - Gets a single transaction for the current user
-    public function getTransaction()
+    public function getTransaction($id)
     {
-
+        $request['id'] = $id;
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // editTransaction - Edits a current user's transaction
-    public function editTransaction()
+    public function editTransaction($params)
     {
-
+        $request['id'] = $params['id'];
+        $request['date'] = $params['date'];
+        $request['amount'] = (float)$params['amount'];
+        $request['transaction_type'] = $params['transaction_type']; // (0= Withdrawal, 1= Deposit, 3= Transfer)
+        $request['account_id'] = (isset($params['account_id']) && $params['account_id'] != 0) ? (int)$params['account_id'] : 0;
+        $request['category_id'] = (isset($params['category_id']) && $params['category_id'] != 0) ? (int)$params['category_id'] : 0;
+        if (isset($params['description']) && $params['description'] != '') $request['description'] = $params['description'];
+        $request['jive'] = (isset($params['jive']) && $params['jive'] != 0) ? $params['jive'] : 0;
+        if (isset($params['from_account_id']) && $params['from_account_id'] != 0) $request['from_account_id'] = $params['from_account_id'];
+        if (isset($params['to_account_id']) && $params['to_account_id'] != 0) $request['to_account_id'] = $params['to_account_id'];
+        if (isset($params['check_num']) && $params['check_num'] != '') $request['check_num'] = $params['check_num'];
+        if (isset($params['memo']) && $params['memo'] != '') $request['memo'] = $params['memo'];
+//        $request['file_attachment'] = ''; // @todo make this work
+        if (isset($params['payee']) && $params['payee'] != '') $request['payee'] = $params['payee'];
+//        $request['split_amounts'] = '' // @todo make this work
+//        $request['split_categories'] = '' // @todo make this work
+//        $request['split_descriptions'] = '' // @todo make this work
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // insertTransaction - Adds a transaction for the current user
-    public function insertTransaction()
+    public function insertTransaction($params)
     {
-
+        $request['date'] = $params['date'];
+        $request['amount'] = (float)$params['amount'];
+        $request['transaction_type'] = $params['transaction_type']; // (0= Withdrawal, 1= Deposit, 3= Transfer)
+        $request['account_id'] = (isset($params['account_id']) && $params['account_id'] != 0) ? (int)$params['account_id'] : 0;
+        $request['category_id'] = (isset($params['category_id']) && $params['category_id'] != 0) ? (int)$params['category_id'] : 0;
+        if (isset($params['description']) && $params['description'] != '') $request['description'] = $params['description'];
+        $request['jive'] = (isset($params['jive']) && $params['jive'] != 0) ? $params['jive'] : 0;
+        if (isset($params['from_account_id']) && $params['from_account_id'] != 0) $request['from_account_id'] = $params['from_account_id'];
+        if (isset($params['to_account_id']) && $params['to_account_id'] != 0) $request['to_account_id'] = $params['to_account_id'];
+        if (isset($params['check_num']) && $params['check_num'] != '') $request['check_num'] = $params['check_num'];
+        if (isset($params['memo']) && $params['memo'] != '') $request['memo'] = $params['memo'];
+//        $request['file_attachment'] = ''; // @todo make this work
+        if (isset($params['payee']) && $params['payee'] != '') $request['payee'] = $params['payee'];
+//        $request['split_amounts'] = '' // @todo make this work
+//        $request['split_categories'] = '' // @todo make this work
+//        $request['split_descriptions'] = '' // @todo make this work
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // deleteTransaction - Deletes a transaction for the current user
-    public function deleteTransaction()
+    public function deleteTransaction($id)
     {
-
+        $request['id'] = $id;
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
     }
 
     // editJive - Changes the jive status of a transaction for the current user
@@ -177,8 +244,19 @@ class clearcheckbook_api
 
     }
 
-    // @todo Add More Functions
+
     // Autocomplete
+
+    // Returns list of descriptions, memos or payees matching the given query
+    function getAutocomplete($params)
+    {
+        $request['type'] = $params['type'];
+        $request['query'] = $params['query'];
+        $return = $this->apiRequest(__FUNCTION__, $request);
+        return $return;
+    }
+
+    // @todo Add More Functions
     // Reports
     // Budgets
     // Reminders
